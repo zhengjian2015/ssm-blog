087c53bcaba05a1d84ef9d11d120bf2a2415f4b3
zhengj <570470242@qq.com>
2018-06-14 09:28:59 +0800

    显示主业

---
+++ src/main/java/com/zj/blog/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.zj.blog.controller;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")  
+    public String IndexView(){  
+        return "/Home/index";
+    }  
+	
+	
+}
